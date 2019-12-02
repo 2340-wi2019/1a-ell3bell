b4753e803ac7b2366339d68e3b3b15bb34c3c45b
ell3bell <58443565+ell3bell@users.noreply.github.com>
2019-12-02 16:35:47 -0600

    Add files via upload

---
+++ assignment01/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready();
+
+$("#first-name").text("Allegra");
+$("#last-name").text("French");
+$("#mcc").html("<i>Metropolitan Community College<i>");
+$("#mcc").css("color", "blue");
+
+$("#hobby").append("<li>Medical Freedom Activist</li>");
+$("#hobby").append("<li>Medical Research</li>");
